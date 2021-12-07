a96409f00f6ae2ae1706513d5e69fb012d18c0c5
Yahia Boussouak <51094855+yahiaBSK@users.noreply.github.com>
2021-12-07 18:12:48 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function openSideBar() {
+  
+  document.getElementById("sidebar").style.left = "0px";
+  document.getElementById("sidebar").style.borderRight = "3px solid #4B36CC";
+
+}
+function closeSideBar() {
+  document.getElementById("sidebar").style.left = "-300px";
+  document.getElementById("sidebar").style.borderRight = "none";
+}
+window.addEventListener('',function opacity(){
+  document.getElementById("follow-us-h2").style.opacity= "1";});
+  
+  
+  
+  
